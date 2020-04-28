d7d9329605cda63aedbb617209747e066740e1da
robsturgeon <rob.sturgeon@twinkl.co.uk>
2020-04-28 09:07:00 +0100

    Initial Commit

---
+++ MakeASwiftUISynth/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MakeASwiftUISynth
+//
+//  Created by Rob Sturgeon on 28/04/2020.
+//  Copyright © 2020 Rob Sturgeon. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
